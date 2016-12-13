4ae9f017ef9bfc1fe08eeab41089d01efe283764
Sameer Ahuja <sameer.ahuja@gmail.com>
2016-12-12 23:47:49 -0800

    Applying workaround for font issue in vi mode

--- fish/config.fish
+++ fish/config.fish
@@ -15,5 +15,5 @@ set fish_plugins theme
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
 
-# Load right prompt
-. ~/.config/fish/functions/rprompt.fish
+# Workaround described in https://github.com/fish-shell/fish-shell/issues/3535#issuecomment-259278388
+function fish_vi_cursor; end
