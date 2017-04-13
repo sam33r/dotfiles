84281337495a75fb9458be43073ddd2cea0b2207
Sameer Ahuja <sameer.ahuja@gmail.com>
2017-04-13 10:59:09 -0700

    Reduce screen off time

--- utils/lock.sh
+++ utils/lock.sh
@@ -7,4 +7,4 @@ convert /tmp/screen_locked.png -scale 10% -scale 1000% /tmp/screen_locked2.png
 rm -f /tmp/screen_locked.png
 i3lock -i /tmp/screen_locked2.png
 
-sleep 60; pgrep i3lock && xset dpms force off
+sleep 30; pgrep i3lock && xset dpms force off
