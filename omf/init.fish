e8bbf4570c7f094d666daa523ac89bd7a416277a
Sameer Ahuja <samahuja@google.com>
2017-06-02 11:09:53 -0700

    Add pyenv to fish paths.

--- omf/init.fish
+++ omf/init.fish
@@ -1,6 +1,8 @@
 # Workaround described in https://github.com/fish-shell/fish-shell/issues/3535#issuecomment-259278388
 function fish_vi_cursor; end
 
+# Set paths.
+set -gx PATH ~/.local/bin ~/pyenv/bin $PATH
 
 # Set environment variables.
 set -gx BROWSER "w3m"
