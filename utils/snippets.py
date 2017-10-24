37cb987503f336362d629619f6f39165f4d8e212
Sameer Ahuja <sameer.ahuja@gmail.com>
2017-10-23 22:31:53 -0700

    Update snippet script to work with newlines.

--- utils/snippets.py
+++ utils/snippets.py
@@ -8,10 +8,13 @@ import datetime
 snippet_map = {
     'date' : datetime.datetime.now().strftime('%b %d %G %I:%M%p '),
     'time' : datetime.datetime.now().strftime('%I:%M%p '),
+    'sign' : 'Best,\nSameer',
 }
 
 keys = '\n'.join(snippet_map.keys())
 result = os.popen('printf "%s" | rofi -dmenu ' % keys)
 
 selected_key = result.read().strip()
-os.system('xdotool type --clearmodifiers -- "%s"' % str(snippet_map[selected_key]))
+
+os.system('sleep 0.1; xdotool type --clearmodifiers "$(printf "%s")"' % str(snippet_map[selected_key]))
+
