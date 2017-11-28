f282d89c522898b15a3313b6a8e9f193ad4fd333
Sameer Ahuja <samahuja@google.com>
2017-11-28 12:15:13 -0800

    Script to add org-mode tasks faster

---
+++ utils/add_task.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Ugly little script for quick-adding org tasks.
+task=$(zenity --entry --text="Enter task title:")
+emacsclient -c -e '(org-capture)' &
+sleep 0.5
+xdotool type t && xdotool key Return
+sleep 0.5
+xdotool type ' wm'
+sleep 0.3
+xdotool type "a$task"
