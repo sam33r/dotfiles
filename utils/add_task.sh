0426526c4437dd8e28727a9317f22c0b129db646
Sameer Ahuja <samahuja@google.com>
2017-12-04 10:10:28 -0800

    Float task window.

--- utils/add_task.sh
+++ utils/add_task.sh
@@ -3,6 +3,8 @@
 task=$(zenity --entry --text="Enter task title:")
 emacsclient -c -e '(org-capture)' &
 sleep 0.5
+i3-msg floating enable
+sleep 0.5
 xdotool type t && xdotool key Return
 sleep 0.5
 xdotool type ' wm'
