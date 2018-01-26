ee7b0bc83ba1897d8d6320f0d8d86357c71b75ba
Sameer Ahuja <samahuja@google.com>
2018-01-25 16:28:08 -0800

    Lightweight script to open chrome via rofi

---
+++ utils/open-chrome.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Utility script to select a URL from Chrome history (via rofi), and then open
+# a new Chrome window with that URL.
+
+mkdir -p ~/tmp
+
+localdb="$HOME/tmp/History"
+
+if [ ! -e "$localdb" ] || [ ! -z "`find $localdb -mmin +5`" ] ; then
+  cp -f  ~/.config/google-chrome/Default/History ~/tmp/
+fi
+
+title_url=`sqlite3 ~/tmp/History "select title,url from urls order by last_visit_time desc limit 5000" | rofi -dmenu`
+
+if [ -z "$title_url" ] ; then
+  exit 0
+fi
+
+url=`echo $title_url | cut -f2 -d'|'`
+google-chrome $url
