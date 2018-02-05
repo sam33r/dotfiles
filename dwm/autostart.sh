854082e99e977af5dd2353ee9d2fb081399753fb
Sameer Ahuja <sameer.ahuja@gmail.com>
2018-02-04 19:35:39 -0800

    Add dwm autostart script

---
+++ dwm/autostart.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+sleep 1
+
+# This enables gtk settings from gnome, required for gtk themes.
+gnome-settings-daemon &
+gnome-keyring-daemon &
+# Speed up the keyboard repeat rate.
+xset r rate 200 51
+# Run compton composition manager.
+compton -f &
+# Start the network management applet.
+nm-applet &
+# Start pulse volume control system tray applet.
+# pasystray &
+# Start feh for wallpaper.
+feh --randomize --bg-fill ~/Wallpapers/* &
+unclutter &
+copyq &
+( sleep 10; /usr/bin/setxkbmap -option "caps:swapescape" ) &
+google-chrome &
+gnome-terminal -e "tmux new -A -s 'persistent'" &
+emacsclient -c --alternate-editor "" &
+redshift &
+start-pulseaudio-x11 &
+
+if [[ -x "./autostart.local.sh" ]]; then
+  ./autostart.local.sh
+fi
+
+while true; do
+   xsetroot -name "$( date )"
+   sleep 10
+done &
