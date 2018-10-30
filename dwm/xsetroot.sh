ab11d33ade4a2fbae264f3e3fae1097e73338c26
Sameer Ahuja <samahuja@google.com>
2018-10-30 16:15:45 -0700

    Show i3blocks in dwm.

--- dwm/xsetroot.sh
+++ dwm/xsetroot.sh
@@ -2,5 +2,8 @@ DATETIME=`date +'%a %F %R'`
 UPTIME=`uptime | sed 's/.*up\s*//' | sed 's/,\s*[0-9]* user.*//' | sed 's/ / /g'`
 VOLUME=$( amixer sget Master | grep -e 'Front Left:' | sed 's/[^\[]*\[\([0-9]\{1,3\}%\).*\(on\|off\).*/\2 \1/' | sed 's/off/MUTE/' | sed 's/on //' )
 BATTERYSTATE=$( acpi -b | awk '{ split($5,a,":"); print substr($3,0,2), $4, "["a[1]":"a[2]"]" }' | tr -d ',' )
+MEMORY=`$HOME/dotfiles/i3blocks/memory | head -2 | tail -1`
+CPU=`$HOME/dotfiles/i3blocks/cpu_usage | head -2 | tail -1`
+TMP=`$HOME/dotfiles/i3blocks/temperature | head -2 | tail -1`
 
-xsetroot -name " ${BATTERYSTATE} | VOL ${VOLUME} | Up ${UPTIME}h | ${DATETIME}"
+xsetroot -name " ${BATTERYSTATE} | CPU $CPU | MEM $MEMORY | VOL ${VOLUME} | TEMP ${TMP} | Up ${UPTIME}h | ${DATETIME}"
