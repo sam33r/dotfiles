41a353d38f885360e65a6f3c082a2ceff70d8d63
Sameer Ahuja <samahuja@google.com>
2018-11-04 19:09:48 -0800

    Use i3blocks script for volume indicator.

--- dwm/xsetroot.sh
+++ dwm/xsetroot.sh
@@ -1,6 +1,6 @@
 DATETIME=`date +'%a %F %R'`
 UPTIME=`uptime | sed 's/.*up\s*//' | sed 's/,\s*[0-9]* user.*//' | sed 's/ / /g'`
-VOLUME=$( amixer sget Master | grep -e 'Front Left:\|Mono:' | sed 's/[^\[]*\[\([0-9]\{1,3\}%\).*\(on\|off\).*/\2 \1/' | sed 's/off/MUTE/' | sed 's/on //' )
+VOLUME=`$HOME/dotfiles/i3blocks/volume`
 BATTERYSTATE=$( acpi -b | awk '{ split($5,a,":"); print substr($3,0,2), $4, "["a[1]":"a[2]"]" }' | tr -d ',' )
 MEMORY=`$HOME/dotfiles/i3blocks/memory | head -2 | tail -1`
 CPU=`$HOME/dotfiles/i3blocks/cpu_usage | head -2 | tail -1`
