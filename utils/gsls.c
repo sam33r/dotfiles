b7a54ca29ce078e083df8dfcb6dc4eea2ebfb388
Sameer Ahuja <samahuja@google.com>
2019-07-19 17:14:10 -0700

    Do color-temperature shifts in the custom GSLS shader.

---
+++ utils/gsls.c
@@ -0,0 +1,40 @@
+// Custom window shader for Compton.
+// This is called from a shell script that replaces {Iteration} with a positive number.
+// Originally based on https://github.com/chjj/compton/issues/530
+
+uniform sampler2D tex;
+void main() {
+    vec4 c = texture2D(tex, gl_TexCoord[0].xy);
+    float y = dot(c.rgb, vec3(0.299, 0.587, 0.114));
+
+    int mode = {Mode}%8;
+
+    switch (mode) {
+      default:
+      case 0: // Default: No transform
+        gl_FragColor = vec4(c.r, c.g, c.b, 1.0);
+        break;
+      case 1: // Color at 2700K
+        gl_FragColor = vec4(c.r, c.g*0.66, c.b*0.34, 1.0);
+        break;
+      case 2: // Color at 2100K
+        gl_FragColor = vec4(c.r, c.g*0.57, c.b*0.13, 1.0);
+        break;
+      case 3: // Color at 1700K
+        gl_FragColor = vec4(c.r, c.g*0.47, 0.0, 1.0);
+        break;
+      case 4: // 2500K Mono
+        gl_FragColor = vec4(y, y*0.63, y*0.28, 1.0);
+        break;
+      case 5: // 2100K Mono
+        gl_FragColor = vec4(y, y*0.57, y*0.13, 1.0);
+        break;
+      case 6: // 1700K Mono
+        gl_FragColor = vec4(y, y*0.47, 0.0, 1.0);
+        break;
+      case 7: // Dark Mode (1700K Mono + Invert)
+        y = 1.0 - y;
+        gl_FragColor = vec4(y, y*0.47, 0.0, 1.0);
+        break;
+    }
+}
