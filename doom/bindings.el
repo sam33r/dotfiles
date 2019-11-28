8cdc76268e07b424a6b23ffdc65b2963ab5a7c35
Sameer Ahuja <samahuja@google.com>
2019-11-27 16:42:35 -0800

    Add some shortcuts to often used files.

--- doom/bindings.el
+++ doom/bindings.el
@@ -1,3 +1,23 @@
 ;;; ~/.doom.d/bindings.el -*- lexical-binding: t; -*-
 
-;; Custom keybindings.
+(setq sa/local-bindings "~/.doom.d/bindings.local.el")
+(if (file-readable-p sa/local-bindings)
+    (load-file sa/local-bindings)
+  (message "No local bindings found."))
+
+;; Shortcuts for oft-used files.
+(map! :leader "F i" (lambda()
+                      (interactive)
+                      (find-file "~/mobile-notes/inbox.org")))
+(map! :leader "F m" (lambda()
+                      (interactive)
+                      (find-file "~/mobile-notes/mobile.org")))
+(map! :leader "F e" (lambda()
+                      (interactive)
+                      (find-file "~/mobile-notes/mail-inbox.org")))
+
+;; Search org files.
+(map! :leader "/" #'sa/search-org-agenda-headings)
+
+(provide 'bindings)
+;;; bindings.el ends here
