1b9ea04a946dac4861062bd7375e0d2c4236349d
Sameer <samahuja@google.com>
2020-01-06 11:05:32 -0800

    Add fuzzy and childframe addons to the ivy module.

--- doom/init.el
+++ doom/init.el
@@ -18,7 +18,9 @@
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       (ivy                ; a search engine for love and life
+        +fuzzy
+        +childframe)
 
        :ui
        ;;deft              ; notational velocity for Emacs
